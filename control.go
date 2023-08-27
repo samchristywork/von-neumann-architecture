c8c21c91eaceb62905595338caf6a79af3061860
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-08-27 18:26:39 -0500

    feat: Add instruction set
    These are the instructions that our CPU will support initially. There
    are some weird things in here, but they are all pretty self-explanatory.

--- control.go
+++ control.go
@@ -4,6 +4,20 @@ import (
 	"fmt"
 )
 
+const (
+	UNKNOWN = iota
+	LOAD_REG_A
+	LOAD_REG_A_C
+	LOAD_REG_B
+	PRINT_REG_A
+	PRINT_REG_B
+	PRINT_REG_C
+	ADD
+	SUB
+	SET_PC
+	JZ
+)
+
 func control() {
 	regA := register8bit{}
 	regB := register8bit{}
