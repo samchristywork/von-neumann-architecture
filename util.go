354a293ffa9a29335cbc935747fdf9730837e5a1
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-08-26 20:55:43 -0500

    feat: Add some utility functions
    These functions just make it easier to deal with certain data types, and
    simplify control flow.

---
+++ util.go
@@ -0,0 +1,31 @@
+package main
+
+func decimel_to_byte(dec int) (out [8]bit) {
+	for i := 7; i >= 0; i-- {
+		out[i] = bit(dec % 2)
+		dec /= 2
+	}
+
+	return out
+}
+
+func byte_to_decimel(in [8]bit) (out int) {
+	out = 0
+	for i := 0; i < 8; i++ {
+		out *= 2
+		out += int(in[i])
+	}
+
+	return out
+}
+
+func match(a [8]bit, b [8]bit) (out bit) {
+	out = 1
+	for i := 0; i < 8; i++ {
+		if a[i] != b[i] {
+			out = 0
+		}
+	}
+
+	return out
+}
