69bb46b521310acf959a6cb880771045a96228a5
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-08-27 18:22:49 -0500

    feat: Add utility functions
    Decimel to nibble is similar to decimel to byte, but uses 4 bits instead
    of 8.

--- util.go
+++ util.go
@@ -19,10 +19,30 @@ func byte_to_decimel(in [8]bit) (out int) {
 	return out
 }
 
+func decimel_to_nibble(dec int) (out [4]bit) {
+	for i := 3; i >= 0; i-- {
+		out[i] = bit(dec % 2)
+		dec /= 2
+	}
+
+	return out
+}
+
 func match(a [8]bit, b [8]bit) (out bit) {
 	out = 1
 	for i := 0; i < 8; i++ {
 		if a[i] != b[i] {
+			out = 0
+		}
+	}
+
+	return out
+}
+
+func isZero(a [8]bit) (out bit) {
+	out = 1
+	for i := 0; i < 8; i++ {
+		if a[i] != 0 {
 			out = 0
 		}
 	}
