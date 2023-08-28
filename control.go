269ee56d38a855515e73a391a438d574698ce448
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-08-27 19:12:57 -0500

    feat: Implement basic controller
    This control system allows me to write primitive programs for my CPU. We
    have loads, arithmetic, and jumps.

--- control.go
+++ control.go
@@ -24,5 +24,41 @@ func control() {
 	regC := register8bit{}
 
 	for i := 0; i < len(input); i++ {
+		instruction := decimel_to_byte(int(input[i]))
+
+		var lookahead [8]bit
+		if i+1 < len(input) {
+			lookahead = decimel_to_byte(int(input[i+1]))
+		}
+
+		if match(instruction, decimel_to_byte(LOAD_REG_A)) == 1 { // Load reg A
+			regA.load(lookahead, 0)
+			regA.load(lookahead, 1)
+			i++
+		} else if match(instruction, decimel_to_byte(LOAD_REG_A_C)) == 1 { // Load reg C into reg A
+			regA.load(regC.read(), 0)
+			regA.load(regC.read(), 1)
+		} else if match(instruction, decimel_to_byte(LOAD_REG_B)) == 1 { // Load reg B
+			regB.load(lookahead, 0)
+			regB.load(lookahead, 1)
+			i++
+		} else if match(instruction, decimel_to_byte(ADD)) == 1 { // Add reg A and B into reg C
+			out, _ := alu(regA.read(), regB.read(), decimel_to_nibble(ALU_ADD), 0)
+			regC.load(out, 0)
+			regC.load(out, 1)
+		} else if match(instruction, decimel_to_byte(SUB)) == 1 { // Subtract reg B from A into reg C
+			out, _ := alu(regA.read(), regB.read(), decimel_to_nibble(ALU_SUB), 0)
+			regC.load(out, 0)
+			regC.load(out, 1)
+		} else if match(instruction, decimel_to_byte(JZ)) == 1 { // Jump if zero
+			if isZero(regC.read()) == 1 {
+				i = byte_to_decimel(lookahead) - 1
+			}
+			i++
+		} else if match(instruction, decimel_to_byte(SET_PC)) == 1 { // Set program counter
+			i = byte_to_decimel(lookahead) - 1
+		} else {
+			fmt.Printf("Unknown instruction: %v\n", instruction)
+		}
 	}
 }
